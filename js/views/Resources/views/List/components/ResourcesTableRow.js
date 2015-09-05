ee16b8d3b01da9b1e4303fc1a02435a6ff6c8f38
Alberto Pedron <pedron.albert@gmail.com>
2015-09-05 02:11:35 -0430

    Add Alertify ResourcesTableRow

--- js/views/Resources/views/List/components/ResourcesTableRow.js
+++ js/views/Resources/views/List/components/ResourcesTableRow.js
@@ -4,6 +4,7 @@ import FontAwesome from 'react-fontawesome';
 import {Button, ButtonToolbar} from 'react-bootstrap'; 
 import {Navigation} from 'react-router';
 import Radium from 'radium';
+import Alertify from 'alertifyjs';
 
 let ResourcesTableRow = React.createClass({
   mixins: [Navigation],
@@ -15,16 +16,23 @@ let ResourcesTableRow = React.createClass({
   },
 
   handleDelete(id) {
-    let confirm = window.confirm('¿Está seguro de que desea eliminar este recurso?');
+    const message = '¿Está seguro de que desea eliminar este recurso?';
 
-    if(confirm) {
-      ResourcesActions
-        .delete
-        .triggerPromise(id)
-        .then((response) => {
-          window.toastr.success('Recurso ha sido eliminado');
-        });
-    }
+    Alertify.defaults.glossary.title = 'Precaucion';
+    Alertify.defaults.glossary.ok = 'SI';
+    Alertify.defaults.glossary.cancel = 'NO';
+
+    Alertify
+      .confirm(message)
+      .set('onok', (closeEvent) => {
+
+        ResourcesActions
+          .delete
+          .triggerPromise(id)
+          .then((response) => {
+            window.toastr.success('Recurso ha sido eliminado');
+          });
+      })
   },
 
   render() {
